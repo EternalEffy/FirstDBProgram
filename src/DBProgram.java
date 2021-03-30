0e86d516fd9f8e583473c7d6ad0e2bb6157d2bac
EternalEffy <effyjeev@gmail.com>
2021-03-30 20:06:20 +0300

    CatalogDBCreator

--- src/DBProgram.java
+++ src/DBProgram.java
@@ -1,17 +1,33 @@
+import org.json.JSONObject;
+
+import java.io.*;
 import java.sql.*;
 
 public class DBProgram {
     private Connection connection;
     private Statement statement;
     private ResultSet resultSet;
+    private File catJson,catDB;
+    private JSONObject jsonObject;
+
+    public void makeSomeFiles(){
+        catJson = new File("Catalog.json");
+        catDB = new File("Catalog.db");
+        connection=null;
+        try {
+            jsonObject = new JSONObject(new BufferedReader( new FileReader(catJson)).readLine());
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        catDB = new File("Catalog.db");
+    }
 
     public void connect(){
         try{
-            connection=null;
             Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:users.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:Catalog.db");
             System.out.println("Connection");
-        } catch (ClassNotFoundException | SQLException e) {
+        }catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
@@ -19,7 +35,7 @@ public class DBProgram {
     public void createDB(){
         try {
             statement = connection.createStatement();
-            statement.execute("CREATE TABLE if not exists 'users' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'name' text, 'age' INT,'score' INT,'level' INT);");
+            statement.execute("CREATE TABLE if not exists 'fileInfo' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'fileName' VARCHAR, 'path' VARCHAR,'extension' VARCHAR,'comment' VARCHAR);");
             System.out.println("Table was created or already exist");
 
         } catch (SQLException throwables) {
@@ -27,25 +43,29 @@ public class DBProgram {
         }
     }
 
-    public void addUser(String name,String age,String score,String level){
+    public void makeCatalog(){
         try {
-            statement.execute(new User(name,age,score,level).requestAdd());
-            System.out.println("Request addUser with UserName: "+name+" |Age:  "+age+" |Score:  "+score+" |Level: "+level+"| was added successful");
+            for(int i=0;i<jsonObject.getJSONArray("fileInfo").length();i++) {
+                statement.execute(new FileInfo(get(i, "fileInfo").getString("fileName"), get(i, "fileInfo").getString("path"), get(i, "fileInfo").getString("extension"), get(i, "fileInfo").getString("comment")).requestAdd());
+            }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
+    public JSONObject get(int index, String listName){
+        return index>jsonObject.getJSONArray(listName).length()-1? null : jsonObject.getJSONArray(listName).getJSONObject(index);
+    }
 
     public void showDB(){
         try {
-            resultSet = statement.executeQuery("SELECT * FROM users");
+            resultSet = statement.executeQuery("SELECT * FROM fileInfo");
             while(resultSet.next())
             {
                 System.out.println( "ID = " + resultSet.getInt("id"));
-                System.out.println( "name = " + resultSet.getString("name"));
-                System.out.println( "age = " + resultSet.getString("age"));
-                System.out.println( "score = " + resultSet.getString("score"));
-                System.out.println( "level = " + resultSet.getString("level"));
+                System.out.println( "fileName = " + resultSet.getString("fileName"));
+                System.out.println( "path = " + resultSet.getString("path"));
+                System.out.println( "extension = " + resultSet.getString("extension"));
+                System.out.println( "comment = " + resultSet.getString("comment"));
                 System.out.println();
             }
             System.out.println("Table was showed");
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         DBProgram program = new DBProgram();
-        Generator generator = new Generator();
+        program.makeSomeFiles();
         program.connect();
         program.createDB();
-        for(int i=0;i<10;i++) {
-            program.addUser(generator.generateName(), generator.generateAge(), generator.generateScore(), generator.generateLevel());
-        }
+        program.makeCatalog();
         program.showDB();
         program.closeDB();
     }
